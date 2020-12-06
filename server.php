1891f13be7f6185dc1de51784c1eaa464d38768a
KhushbooJain2 <69629066+KhushbooJain2@users.noreply.github.com>
2020-12-06 15:34:16 +0530

    Update server.php

--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 <?php
-$db=mysqli_connect('database-1.ce3o1dpuwdox.us-east-1.rds.amazonaws.com','admin',"admin123","student_details");
+$db=mysqli_connect('database-1.ce3o1dpuwdox.us-east-1.rds.amazonaws.com','admin',"admin123","student");
 if(isset($_POST['reg_user']))
 {
 	$roll_number = mysqli_real_escape_string($db, $_POST['roll_number']);
